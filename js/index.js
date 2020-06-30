c73c958f218cacb4d7475b639a837235999f2241
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2020-06-30 09:16:23 +0200

    Initial commit
    Prototype / proof of concept is working

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	
+	setTimeout(lin1, 1000);
+
+	function lin1() {
+		/*	Status: Prototype
+	
+		*/	
+
+		$("#lin1 h2, #lin1 p").css('visibility', 'visible');
+
+		setTimeout(lin2, 1000);
+	}
+
+		function lin2() {
+		/*	Status: Prototype
+	
+		*/	
+
+		$("#lin2 h2, #lin2 p").css('visibility', 'visible');
+
+		//setTimeout(lin2, 1000);
+	}
+});
