63a78fbcd2335a10b01024883430db175d9f4aad
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2020-07-07 21:51:38 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,24 +1,61 @@
 $(document).ready(function() {
-	
-	setTimeout(lin1, 1000);
+	// https://www.w3schools.com/js/js_timing.asp
 
-	function lin1() {
-		/*	Status: Prototype
-	
-		*/	
+	/*$(".linus .timeline-content").each(function(index, el) {
+		
+		$(this).delay(index * 1000).fadeIn(1000);
+	});*/
 
-		$("#lin1 h2, #lin1 p").css('visibility', 'visible');
+	/*	fadeIn() doesn't show hidden elements when they are visibility: hidden.
+		It will show display: none.
+			Mother fuckers.
+		https://stackoverflow.com/questions/2992611/jquery-showing-a-hidden-item-with-a-fadein
+	*/
 
-		setTimeout(lin2, 1000);
+	// Automatically generates Linus's IDs and adds them to the correct div
+	let ids = $(".timeline-content div");
+
+	const idX = [];
+	for (let i = 0; i < ids.length; i++) {
+
+		idX[i] = `lin${i}`;
+		$(ids[i]).attr('id', idX[i] );
 	}
 
-		function lin2() {
+	// function main runs every 2 seconds.. but only actually once.. Perhaps not the correct method after all..?
+	//setInterval( main, 2000 );
+	// https://javascript.info/settimeout-setinterval
+	setTimeout(main, 2000, idX);
+
+	function main(idX) {
+		// 
+		linusShow(idX);
+		// If works, Add helenShow() as awell as third-col
+	}
+
+	function linusShow(idX) {
+		// 
+
+		for (let i = 0; i < idX.length; i++) {
+			$( "#" + idX[i] ).fadeIn( 3000 ); // default = 400, A higher value => slower fadeIn
+
+			
+		}
+	}
+
+	/*setTimeout(lin0, 1000);
+
+	function lin0() {
 		/*	Status: Prototype
-	
+			
 		*/	
 
-		$("#lin2 h2, #lin2 p").css('visibility', 'visible');
+		//$( "#lin0" ).fadeIn( 2000 ); // default = 400, A higher value => slower fadeIn
 
-		//setTimeout(lin2, 1000);
-	}
+		//$("#lin0 h2, #lin0 p").css('visibility', 'visible');
+
+		/*	Not what I want. I want the text to fadeIn!
+		*/
+	//}
+
 });
